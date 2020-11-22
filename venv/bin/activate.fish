8a075700075c468b4c32b3ccdd5118e545603d3b
Juan Camilo Lopez <juclopezso@unal.edu.co>
2020-11-21 21:16:06 -0500

    fix venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/juancho/Development/microblog_flask_course/venv"
+set -gx VIRTUAL_ENV "/home/juancho/Development/courses/flask_course_microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
