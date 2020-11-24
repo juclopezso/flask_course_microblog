5b43c800c05891591461f6844a40707f17902b66
Juan Camilo Lopez <juclopezso@unal.edu.co>
2020-11-24 12:31:26 -0500

    Chapter 5. User login and registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # __name__ is predefined variable
 # flask uses the location of the module her
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# initialization of extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+# show flask-login where is the login view function 
+login.login_view = 'login'
 
 # The bottom import is a workaround to circular imports
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
-from datetime import datetime
 from app import db
+from app import login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
 
 # User has_many Posts
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,6 +19,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User: {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -25,3 +35,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post: {}>'.format(self.body)
+
+# Because Flask-Login knows nothing about databases, it needs the application's help in loading a user
+# The user loader is registered with Flask-Login with the decorator
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
